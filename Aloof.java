65ba760b9391105cbf79b83cc22947d640ab560b
Louisa Reese <louisa.e.reese@gmail.com>
2016-05-11 10:42:53 -0600

    prints with argument

--- Aloof.java
+++ Aloof.java
@@ -1,6 +1,6 @@
 // class has same name as file name
 public class Aloof {
   public static void main(String [] args) {
-    System.out.println(", I guess...");
+    System.out.println(args[0] + ", I guess...");
   }
 }
