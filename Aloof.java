2d4c7466621b1628ff660d206f956c7b203c9e0e
Louisa Reese <louisa.e.reese@gmail.com>
2016-05-11 10:37:40 -0600

    new file, practice with creating class

---
+++ Aloof.java
@@ -0,0 +1,7 @@
+// class has same name as file name
+public class Aloof {
+
+  public static void main(String [] args) {
+
+  }
+}
--- Hello.java
+++ Hello.java
@@ -1,7 +1,11 @@
 public class Hello {
 
   public static void main(String [] args) {
-    System.out.println("Hello, Java!");
+    System.out.println("Hello, World!");
   }
-  
+
 }
+// console.log('hello world')
+
+// aloof fn
+// "hello, world" >> "hello, world, i guess..."
